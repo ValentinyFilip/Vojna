539927f5a9d9ab4be7ad61f1bf76b1865af24177
ValentinyFilip <80767029+ValentinyFilip@users.noreply.github.com>
2023-05-22 18:45:52 +0200

    buttons and labels

--- Vojna/Form1.Designer.cs
+++ Vojna/Form1.Designer.cs
@@ -20,18 +20,41 @@ partial class Form1
         base.Dispose(disposing);
     }
 
+    private void newButtoN_Click(object sender, System.EventArgs e) {
+        MessageBox.Show("New button clicked");
+    }
+
+    private void InitiliazeViewPort() {
+        Button newButton = new Button();
+        Label winPlayer = new Label();
+        Label winAI = new Label();
+        
+        newButton.Click += new EventHandler(newButtoN_Click);
+        newButton.Text = "Klikni mě";
+        newButton.Location = new Point(350, 300);
+        newButton.Size = new Size(200, 50);
+
+        winPlayer.Location = new Point(100, 50);
+        winPlayer.Text = "0";
+        winAI.Text = "0";
+        winAI.Location = new Point(700, 50);
+        
+        this.Controls.Add(newButton);
+        this.Controls.Add(winAI);
+        this.Controls.Add(winPlayer);
+    }
+    
     #region Windows Form Designer generated code
 
     /// <summary>
     ///  Required method for Designer support - do not modify
     ///  the contents of this method with the code editor.
     /// </summary>
-    private void InitializeComponent()
-    {
+    private void InitializeComponent() {
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        this.Text = "Vojna";
     }
 
     #endregion
